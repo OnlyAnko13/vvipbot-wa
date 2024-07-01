e6135f7c650b1961852e6daab21dc2e1bbd8ecfc
OnlyAnko13 <akunvscoh@gmail.com>
2024-07-01 21:13:14 +0800

    index.js

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
+const BotName = 'Anko Bot'; 
+const instagram = 'yk.ankoo- r3nzzzzz'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Ahnaf13\n' // Nama kamu
+            + 'ORG:Ankoo;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6285756590894:+62 857-5659-0894\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
